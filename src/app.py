153bdf1b71e9fd81c9697d5b715a69a2b7395a31
Jhenrymm <jhenrymamani51@gmail.com>
2023-08-31 20:58:12 -0300

    cambios realizados

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 from config import config
+from flask_mysqldb import MySQL
 
 app=Flask(__name__)
+
+db=MySQL(app)
 
 @app.route('/')
 def index():
@@ -10,11 +13,24 @@ def index():
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        print(request.form['username'])
-        print(request.form['password'])
+        #print(request.form['username'])
+        #print(request.form['password'])
+        user = User(0, request.form['username'], request.form['password'])
+        logged_user = ModelUser.login(db,user)
+
+        if logged_user != None:
+            if logged_user.password:
+                return redirect(url_for('perfilcliente'))
+            else:
+                flash("Credenciales incorrectas...")
+            return render_template ('auth/login.html')
+        else:
+            flash("Usuario no existente...")
+            return render_template ('auth/login.html')
+
         return render_template('auth/login.html')
     else:
-        return render_template('auth/vcpregunta.html')
+        return render_template('auth/login.html')
         
 
 if __name__=='__main__':
---
+++ src/models/ModelUser.py
@@ -0,0 +1,17 @@
+class ModelUser():
+
+    @classmethod
+    def login(self, db, user):
+        try:
+            cursor = db.connection.cursor()
+            sql= """SELECT id, username, password, fullname FROM usuario
+             WHERE username = '{}'""".format(user.username)
+            cursor.execute(sql)
+            row = cursor.fetchone()
+            if row != None:
+                user = User(row[0], row[1], User.check_password_hash(row[2], user.password), row[3])
+                return user
+            else:
+                return None
+        except Exception as ex:
+            raise Exception(ex)
